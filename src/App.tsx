13e754ce018d3bd46c9051855b3007b8a967edc2
Simon <sstur@me.com>
2023-10-16 19:17:01 -0700

    Add ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
