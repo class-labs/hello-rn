69047ff13f10074e29c9683f7f11445cf83d2aaa
Simon <sstur@me.com>
2023-10-18 14:53:36 -0700

    Implement sliding animation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+  Easing,
+} from "react-native-reanimated";
+
+import { Button } from "../components/Button";
 
 export function HomeScreen() {
+  const position = useSharedValue(0);
+
+  const specialStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: position.value }],
+    };
+  });
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        padding: 20,
+      }}
+    >
+      <View style={{ flex: 1 }}>
+        <Animated.View
+          style={[
+            specialStyle,
+            { width: 120, height: 120, backgroundColor: "#4E46DC" },
+          ]}
+        />
+      </View>
+      <Button
+        onPress={() => {
+          position.value = withSequence(
+            withTiming(200, {
+              duration: 500,
+              easing: Easing.inOut(Easing.quad),
+            }),
+            withTiming(0, {
+              duration: 500,
+              easing: Easing.inOut(Easing.quad),
+            }),
+          );
+        }}
+      >
+        Animate
+      </Button>
     </View>
   );
 }
