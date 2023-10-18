9fd73f33c2af9e77d3b74620728683583bec8feb
Simon <sstur@me.com>
2023-10-18 11:24:13 -0700

    Initial pass at react-hook-form

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,40 +1,90 @@
-import { ScrollView, TextInput as RNTextInput } from "react-native";
+import { ScrollView, TextInput as RNTextInput, Alert } from "react-native";
 import { TextInput } from "../components/TextInput";
 import { Button } from "../components/Button";
 import { Text } from "../components/Text";
 import { Link } from "../components/Link";
 import { Form } from "../components/Form";
 import { useRef } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 export function SignupScreen() {
   const inputEmailRef = useRef<RNTextInput>(null);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      emailAddress: "",
+      phoneNumber: "",
+      password: "",
+      passwordConfirm: "",
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("Submitted:", data);
+    Alert.alert("Thanks!");
+  });
+
   return (
     <ScrollView style={{ backgroundColor: "white" }}>
       <Form style={{ gap: 20, padding: 20 }}>
-        <TextInput
-          placeholder="Enter your name"
-          autoCorrect={false}
-          returnKeyType="next"
-          onSubmitEditing={() => {
-            inputEmailRef.current?.focus();
+        <Controller
+          control={control}
+          name="name"
+          rules={{
+            required: true,
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Enter your name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCorrect={false}
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                inputEmailRef.current?.focus();
+              }}
+            />
+          )}
         />
-        <TextInput
-          ref={inputEmailRef}
-          placeholder="Enter your email"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          spellCheck={false}
-          autoCorrect={false}
-          returnKeyType="next"
+        {errors.name && <Text style={{ color: "red" }}>This is required.</Text>}
+
+        <Controller
+          control={control}
+          name="emailAddress"
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              ref={inputEmailRef}
+              placeholder="Enter your email"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              spellCheck={false}
+              autoCorrect={false}
+              returnKeyType="next"
+            />
+          )}
         />
+        {errors.emailAddress && (
+          <Text style={{ color: "red" }}>This is required.</Text>
+        )}
+
         <TextInput placeholder="Enter your phone number" />
         <TextInput placeholder="Enter your password" secureTextEntry={true} />
         <TextInput
           placeholder="Enter your password again"
           secureTextEntry={true}
         />
-        <Button>Submit</Button>
+        <Button onPress={onSubmit}>Submit</Button>
         <Text>
           Don't have an account? <Link to="Signup">Sign up</Link>
         </Text>
