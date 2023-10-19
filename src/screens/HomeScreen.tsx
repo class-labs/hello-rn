d7ff19e1e47c4bb6b84bec426a69cd083087abba
Simon <sstur@me.com>
2023-10-19 12:25:11 -0700

    Add image instead of square

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -31,15 +31,17 @@ export function HomeScreen() {
     <View
       style={{
         flex: 1,
-        padding: 20,
       }}
     >
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <GestureDetector gesture={pan}>
-          <Animated.View
+          <Animated.Image
+            source={{
+              uri: "https://github.com/sstur/sstur/assets/369384/365fb8f3-4082-486a-9d31-eba4cf89dd11",
+            }}
             style={[
               specialStyle,
-              { width: 120, height: 120, backgroundColor: "#4E46DC" },
+              { width: "100%", aspectRatio: 1, backgroundColor: "#4E46DC" },
             ]}
           />
         </GestureDetector>
