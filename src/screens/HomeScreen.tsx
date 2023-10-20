4809a0ec265781efc6e2660d03f3e4692ce53886
Simon <sstur@me.com>
2023-10-20 13:02:20 -0700

    Task 18 solution

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,6 @@
-import { Text, View } from "react-native";
-import { gql } from "@apollo/client";
-
-// Task 18
-// Query the list of posts from GraphQL and display them on the screen in a
-// simple FlatList
+import { FlatList, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { Text } from "../components/Text";
 
 const GET_POSTS = gql`
   query GetPosts {
@@ -12,6 +9,7 @@ const GET_POSTS = gql`
       author {
         id
         name
+        username
       }
       content
       likeCount
@@ -20,9 +18,39 @@ const GET_POSTS = gql`
 `;
 
 export function HomeScreen() {
+  const { data, loading, error } = useQuery(GET_POSTS);
+
+  if (error) {
+    return <Text style={{ color: "red" }}>{String(error)}</Text>;
+  }
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  const posts = data.posts ?? [];
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
+    <FlatList
+      style={{ flex: 1, backgroundColor: "#ddd" }}
+      contentContainerStyle={{ padding: 20, gap: 20 }}
+      data={posts}
+      keyExtractor={(post) => post.id}
+      renderItem={({ item: post }) => (
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: "white",
+            borderRadius: 5,
+            gap: 5,
+          }}
+        >
+          <Text style={{ fontWeight: "500" }}>
+            {post.author.name} (@{post.author.username})
+          </Text>
+          <Text>{post.content}</Text>
+        </View>
+      )}
+    />
   );
 }
