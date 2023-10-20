6679136c000cd3c57561e637c6b6a656e98d4ca5
Simon <sstur@me.com>
2023-10-20 08:56:45 -0700

    Use Apollo useMutation for login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -3,34 +3,39 @@ import { Form } from "../components/Form";
 import { TextInput } from "../components/TextInput";
 import { Button } from "../components/Button";
 import { useState } from "react";
-import { login } from "../api/auth";
 import { Text } from "../components/Text";
 import { useSession } from "../support/SessionProvider";
-import { Alert } from "react-native";
+import { gql, useMutation } from "@apollo/client";
+
+const LOGIN = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        id
+        name
+        username
+      }
+    }
+  }
+`;
 
 export function LoginScreen() {
   const navigation = useNavigation<any>();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState<Error | null>(null);
-  const [loading, setLoading] = useState(false);
   const { setSession } = useSession();
 
+  const [login, { loading, error }] = useMutation(LOGIN, {
+    onCompleted: (data) => {
+      const session = data.login;
+      setSession(session);
+      navigation.navigate("Main");
+    },
+  });
+
   const onSubmit = () => {
-    setLoading(true);
-    login(username, password)
-      .then((result) => {
-        if (result) {
-          // setSession(result);
-          // navigation.navigate("Main");
-          Alert.alert(JSON.stringify(result));
-        } else {
-          setError(new Error("Invalid username or password"));
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    login({ variables: { username, password } });
   };
 
   return (
