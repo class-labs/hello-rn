50a94fabae89edac12afdf66c8a7a7e5d5a57c61
Simon <sstur@me.com>
2023-10-19 21:26:44 -0700

    Add icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { Home } from "lucide-react-native";
 
 export function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
+      <Home />
     </View>
   );
 }
