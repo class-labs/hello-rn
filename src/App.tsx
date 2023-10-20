d10622aa2f3194a3cc58cee6fc65df98ad494a4d
Simon <sstur@me.com>
2023-10-20 09:19:33 -0700

    Setup for task 17

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,30 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { MainScreen } from "./screens/MainScreen";
 import { LoginScreen } from "./screens/LoginScreen";
 import { SessionProvider } from "./support/SessionProvider";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const Stack = createNativeStackNavigator();
 
+const client = new ApolloClient({
+  uri: "https://chatter.web-api.dev/graphql",
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
-    <SessionProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen
-            name="Main"
-            options={{ headerShown: false }}
-            component={MainScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen
+              name="Main"
+              options={{ headerShown: false }}
+              component={MainScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
