85157f15db078a892994160df4acf1b0e0f5a6eb
Simon <sstur@me.com>
2024-03-23 16:28:40 -0700

    Add ts-reset and some stuff to global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,42 @@
-/* eslint-disable no-var */
+/* eslint-disable no-var, @typescript-eslint/consistent-type-definitions */
+import '@total-typescript/ts-reset';
+
 import { TFn } from '../support/intl';
 
 declare global {
   // NOTE: Need to use `var` and not `const` here for weird TS reasons.
   // Source https://stackoverflow.com/a/69429093
   var t: TFn;
+
+  // Override the types for Object.keys() and Object.entries() to be more
+  // helpful in cases where the keys are of a known set.
+  interface ObjectConstructor {
+    keys<T>(
+      o: T,
+    ): T extends Record<string, unknown>
+      ? Array<keyof T & string>
+      : Array<string>;
+
+    entries<T>(
+      o: T,
+    ): T extends Record<string, infer U>
+      ? Array<[keyof T & string, U]>
+      : T extends ArrayLike<infer U>
+        ? Array<[string, U]>
+        : Array<[string, unknown]>;
+  }
+
+  // ===
+  // The rest of these are some helpful global utility types
+  // ===
+
+  type ValueOf<T extends object> = T[keyof T];
+
+  // This is used to "expand" an intersection of two or more objects when
+  // displayed in tooltips, for example `Expand<{ a: string } & { b: string }>`
+  // will expand to `{ a: string, b: string }`
+  // Reference: https://stackoverflow.com/a/57683652
+  type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+  type TimeoutId = ReturnType<typeof setTimeout>;
 }
