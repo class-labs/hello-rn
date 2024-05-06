507c31ebea113d96c251eb3cedb1429acb89895b
Simon <sstur@me.com>
2024-05-05 21:04:46 -0700

    Dark mode

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,8 +1,29 @@
 import type { ReactNode } from 'react';
-import { TamaguiProvider } from 'tamagui';
+import { View } from 'react-native';
+import {
+  DarkTheme,
+  ThemeProvider as NavigationThemeProvider,
+} from '@react-navigation/native';
+import { TamaguiProvider, Theme } from 'tamagui';
 
 import config from '../config/tamagui.config';
 
 export function ThemeProvider(props: { children: ReactNode }) {
-  return <TamaguiProvider config={config}>{props.children}</TamaguiProvider>;
+  return (
+    <TamaguiProvider config={config}>
+      <Theme name="dark">
+        <NavigationThemeProvider value={DarkTheme}>
+          <View
+            style={{
+              flex: 1,
+              // TODO: Use a constant here or get it from our theme config
+              backgroundColor: '#0E1116',
+            }}
+          >
+            {props.children}
+          </View>
+        </NavigationThemeProvider>
+      </Theme>
+    </TamaguiProvider>
+  );
 }
