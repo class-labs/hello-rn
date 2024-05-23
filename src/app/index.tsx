960a356185eba48c7cf9bdf02e657ad69ff85097
Simon <sstur@me.com>
2024-05-23 07:12:24 -0700

    Add react-native-svg and @tamagui/lucide-icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Home } from '@tamagui/lucide-icons';
 import { Button, YStack } from 'tamagui';
 
 export default function App() {
   return (
     <YStack flex={1} justifyContent="center" alignItems="center">
-      <Button>{t('Hello world!')}</Button>
+      <Button icon={<Home />}>{t('Hello world!')}</Button>
     </YStack>
   );
 }
