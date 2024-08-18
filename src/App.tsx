f74ed0acc0e90eb358df142939b102eec8064198
Simon <sstur@me.com>
2024-08-18 16:35:24 -0700

    Move files into src directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
