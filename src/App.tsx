422ce5cb8c505b70cce47b10ca4ba6d9aa70eb83
Simon <sstur@me.com>
2024-08-18 20:24:20 -0700

    Move files into src directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
+export function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import {App} from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
