5ea7b8a70ab9044d3c49e4ceb836952672615e21
Simon <sstur@me.com>
2024-08-20 09:16:48 -0700

    Example text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export function App() {
+  const [firstName, setFirstName] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, paddingHorizontal: 20 }}>
+        <Text>Enter your first name:</Text>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderColor: "#9d9d9d",
+            borderRadius: 4,
+            padding: 10,
+            minWidth: 180,
+          }}
+          placeholder="Enter your first name"
+          value={firstName}
+          onChangeText={(newValue) => {
+            setFirstName(newValue);
+          }}
+        />
+        <View style={{ alignItems: "flex-end" }}>
+          <Button
+            onPress={() => {
+              Alert.alert(`Hello "${firstName}"`);
+              setFirstName("");
+            }}
+            title="Submit"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
