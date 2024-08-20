37b8be83c314d16fa22ed2486b53c0e3f87aa5f1
Simon <sstur@me.com>
2024-08-20 13:27:00 -0700

    Add Signup form to react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { HomeScreen } from "./screens/HomeScreen";
+import { SignupScreen } from "./screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
 
 export function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Signup">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/SignupScreen.tsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export function SignupScreen() {
+  const navigation = useNavigation();
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={{ paddingHorizontal: 20, gap: 10 }}>
+      <View style={{ marginVertical: 14 }}>
+        <Text style={{ fontSize: 32 }}>Sign in to your account</Text>
+      </View>
+      <Text style={styles.textFieldLabel}>Email address</Text>
+      <TextInput
+        style={styles.textFieldInput}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+        spellCheck={false}
+        value={emailAddress}
+        onChangeText={setEmailAddress}
+      />
+      <Text style={styles.textFieldLabel}>Password</Text>
+      <TextInput
+        style={styles.textFieldInput}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <View style={styles.horizontalFieldRow}>
+        <View style={{ flexDirection: "row", gap: 8, alignItems: "center" }}>
+          <View style={styles.checkBox} />
+          <Text style={{ fontSize: 16 }}>Remember me</Text>
+        </View>
+        <Text style={{ fontSize: 16, color: "#4E46DC", fontWeight: "bold" }}>
+          Forgot password?
+        </Text>
+      </View>
+      <Pressable
+        style={styles.submitButton}
+        onPress={() => {
+          if (emailAddress === "bob@example.com" && password === "123") {
+            // TODO: Setup TypeScript with React Navigation
+            // @ts-ignore
+            navigation.navigate("Home");
+          } else {
+            Alert.alert("Invalid login");
+          }
+        }}
+      >
+        <Text style={styles.submitButtonText}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  textFieldLabel: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  textFieldInput: {
+    borderWidth: 1,
+    borderColor: "#6b7280",
+    padding: 10,
+    borderRadius: 8,
+  },
+  horizontalFieldRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  checkBox: {
+    width: 18,
+    height: 18,
+    borderWidth: 1,
+    borderColor: "#6b7280",
+    borderRadius: 3,
+  },
+  submitButton: {
+    padding: 14,
+    backgroundColor: "#4E46DC",
+    borderRadius: 8,
+  },
+  submitButtonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
