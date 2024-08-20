ec3969941abdffc5e792b7c6134baffe2464079d
Simon <sstur@me.com>
2024-08-20 13:52:39 -0700

    Task 9 solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { AboutScreen } from "./screens/AboutScreen";
 import { HomeScreen } from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
@@ -10,6 +11,7 @@ export function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,15 +1,15 @@
 import { Button, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-export function HomeScreen() {
+export function AboutScreen() {
   const navigation = useNavigation();
   return (
     <View style={{ alignItems: "center", padding: 20 }}>
       <Button
-        title="Go to About"
+        title="Go to Home"
         onPress={() => {
           // @ts-ignore
-          navigation.navigate("About");
+          navigation.navigate("Home");
         }}
       />
     </View>
