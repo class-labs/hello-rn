83688c3f428109aa89580ffdb4a78978e3b803da
Simon <sstur@me.com>
2024-09-30 16:11:43 -0700

    Add productionApiHost and constants

---
+++ src/support/constants.ts
@@ -0,0 +1,22 @@
+import Constants from 'expo-constants';
+
+export const API_BASE_URL = getApiBaseUrl();
+
+function getApiBaseUrl(): string {
+  const apiHostFromEnv = process.env.EXPO_PUBLIC_API_HOST;
+  if (apiHostFromEnv) {
+    return apiHostFromEnv;
+  }
+  const { expoConfig } = Constants;
+  if (__DEV__) {
+    // In dev, hostUri is available from mobile (but not web)
+    const host = expoConfig?.hostUri ?? 'localhost';
+    const hostname = host.split(':')[0] ?? '';
+    return `http://${hostname}:8000`;
+  }
+  const host: unknown = expoConfig?.extra?.productionApiHost;
+  if (typeof host !== 'string') {
+    throw new Error('Missing "productionApiHost" in app.json');
+  }
+  return host;
+}
