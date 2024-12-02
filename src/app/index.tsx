918784532eb90a0c6dd01bf660912bac6cb1764b
Simon <sstur@me.com>
2024-12-02 13:19:19 -0800

    Add @tamagui/lucide-icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
+import { Home } from '@tamagui/lucide-icons';
+
 import { Button, Text, YStack } from '../components/core';
 
 export default function App() {
   return (
     <YStack flex={1} justifyContent="center" alignItems="center" gap="$4">
-      <Button>{t('Hello world!')}</Button>
+      <Button icon={<Home />}>{t('Hello world!')}</Button>
       <Text>{t('Hello world!')}</Text>
     </YStack>
   );
